a33570ff2848a5eaf43e8fd15d2e3f63372c8ba0
ChickenChamp71 <chickensandsharks@gmail.com>
2023-07-03 22:04:52 -0700

    wheee

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const rootContainer = document.getElementById("root")
-
-const root = ReactDOM.createRoot(rootContainer);
-
-root.render(<App />);
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
